7ef832faedca0c4f9b5e46c87a7778c7484b5fd6
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-10-17 22:48:56 +0300

    Установка vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from "@/router"
 import axios from 'axios'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 
 Vue.config.productionTip = false
@@ -16,5 +17,6 @@ if (token) {
 new Vue({
   router,
   store,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
