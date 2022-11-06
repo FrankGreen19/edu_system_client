fe65c0845220b5f9a1db4e79fe6c65d1f989d365
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-11-06 19:38:53 +0300

    явно указан адрес приложения

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    host: '127.0.0.1',
+    port: 8082
+  },
   transpileDependencies: [
     'vuetify'
   ]
