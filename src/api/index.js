c110420487735be548e74c1d47786bfdff7368a4
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-11-06 19:40:56 +0300

    Дефолтный экземпляр аксиоса отправляет кукис всегда. Интерцептор для отлова неавторизованных запросов

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,49 @@
 import axios from "axios";
+import store from "/src/store"
 
 const defaultConfig = {
     baseURL: process.env.VUE_APP_API_URL,
+    withCredentials: true,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('token')
+            ? `Bearer ${localStorage.getItem('token')}`
+            : null
     }
-}
-
-const token = localStorage.getItem('token')
-if (token) {
-    defaultConfig.headers['Authorization'] = `Bearer ${token}`
 }
 
 const loginConfig = {
     baseURL: process.env.VUE_APP_API_URL,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     }
 }
 
-export const DefaultApiInstance = axios.create(defaultConfig);
-export const LoginApiInstance = axios.create(loginConfig);
+const DefaultApiInstance = axios.create(defaultConfig);
+const LoginApiInstance = axios.create(loginConfig);
+
+DefaultApiInstance.interceptors.response.use(
+     (response) => {
+        // Любой код состояния, находящийся в диапазоне 2xx, вызывает срабатывание этой функции
+        return response;
+    },
+    async (error) => {
+        let originalRequest = error.config;
+        originalRequest._isRetry = true;
+
+        if (error.code === 'ERR_BAD_REQUEST') {
+            store.dispatch('refresh')
+                .then(() => {
+                    originalRequest.headers['Authorization'] = localStorage.getItem('token');
+                    return DefaultApiInstance.request(originalRequest);
+                })
+                .catch(() => {
+                    store.dispatch('logout');
+                })
+        }
+
+        return Promise.reject(error);
+     }
+);
+
+export {DefaultApiInstance, LoginApiInstance};
