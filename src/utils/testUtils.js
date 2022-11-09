eeb905c00a423f61e4ebaf81843f278d7a3f3521
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-11-09 23:44:01 +0300

    Константы теста вынесены в отдельный файл

---
+++ src/utils/testUtils.js
@@ -0,0 +1,10 @@
+export const TEST_UTILS = {
+    TEST_TYPES: {
+        GENERATED: 18,
+        CUSTOM: 17,
+    },
+    QUESTION_CATEGORY_RADIOS: [
+        {id: 1, title: 'Общая'},
+        {id: 2, title: 'Собственная'}
+    ],
+}
