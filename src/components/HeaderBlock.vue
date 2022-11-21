68c26600997173dcc691a4208eb9f0d36a73af29
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-11-21 23:23:30 +0300

    Правки шапки

--- src/components/HeaderBlock.vue
+++ src/components/HeaderBlock.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app-bar rounded class="mb-5">
-    <v-toolbar-title>
+    <v-toolbar-title @click="$router.push('/')">
       <span style="color: #1976D2">Сайтик с тестиками</span>
     </v-toolbar-title>
     <v-spacer />
-    <v-btn class="mr-2 white--text" rounded color="orange lighten-1" @click="$router.push('login')">Пройти тест</v-btn>
+    <v-btn class="mr-2 white--text" rounded color="orange lighten-1" @click="$router.push('login')">Войти</v-btn>
   </v-app-bar>
 </template>
 
