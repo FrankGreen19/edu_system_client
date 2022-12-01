d55328c61bc0a9013ef4ae21787d8a70762add8f
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-12-01 21:20:42 +0300

    Правки футера

--- src/components/FooterBlock.vue
+++ src/components/FooterBlock.vue
@@ -11,19 +11,20 @@
       >
         <v-btn
             v-for="link in links"
-            :key="link"
+            :key="link.title"
             color="white"
             text
             rounded
             class="my-2"
+            @click="$router.push(link.route)"
         >
-          {{ link }}
+          {{ link.title }}
         </v-btn>
         <v-col
             class="primary lighten-2 py-4 text-center white--text"
             cols="12"
         >
-          {{ new Date().getFullYear() }} — <strong>Подвал стайтика с тестиками</strong>
+          {{ new Date().getFullYear() }} — <strong>Подвал сайтика с тестиками</strong>
         </v-col>
       </v-row>
     </v-footer>
@@ -35,12 +36,14 @@ export default {
 
   data: () => ({
     links: [
-      'Home',
-      'About Us',
-      'Team',
-      'Services',
-      'Blog',
-      'Contact Us',
+      {
+        title: 'На главную',
+        route: '/',
+      },
+      {
+        title: 'Об авторе',
+        route: '/'
+      },
     ],
   }),
 }
